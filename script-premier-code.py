782895adefda351e283a8adf20635558750c647f
Hyacinthe ABANDA <hyacinthabanda@gmail.com>
2023-01-31 17:16:32 +0100

    premier code python

---
+++ script-premier-code.py
@@ -0,0 +1,11 @@
+#coding:utf-8
+
+# Un commentaire d'une seule ligne
+
+""" 
+Test d'affichage du Print()
+\t pour faire la tabulation au niveau de l'affichage du texte après interprétation
+\n pour aller à la ligne
+"""
+
+print("\tBonjour tout le monde")
